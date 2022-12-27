ca5ef1a2208b1ab4f47f6658c5e49db8f5b207c8
myandue <97776790+myandue@users.noreply.github.com>
2022-12-27 13:30:26 +0900

    22.12.27

---
+++ src/main/java/com/example/fluxtest/MySub.java
@@ -0,0 +1,39 @@
+package com.example.fluxtest;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public class MySub implements Subscriber<Integer> {
+
+    private Subscription s;
+    private int bufferSize = 3;
+
+    @Override
+    public void onSubscribe(Subscription s) {
+        System.out.println("구독자: 구독 정보 잘 받았어");
+        this.s=s;
+        System.out.println("구독자: 나 이제 신문 3개씩 줘");
+        s.request(bufferSize); //신문 한개씩 매일매일 줘! (백프레셔) 소비자가 한번에 처리할 수 있는 개수를 요청
+    }
+
+    @Override
+    public void onNext(Integer integer) {
+        System.out.println("onNex(): " + integer);
+        bufferSize--;
+        if(bufferSize==0){
+            System.out.println("하루 지남");
+            bufferSize=3;
+            s.request(bufferSize);
+        }
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        System.out.println("구독중 에러");
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println("구독 완료");
+    }
+}
